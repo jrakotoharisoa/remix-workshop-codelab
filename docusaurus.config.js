824e3af532d3cbf2668a98ac20f10ef39e9d1840
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2023-02-06 13:46:26 +0100

    ✏️ fix typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Remix Workshop codelabas",
+  title: "Remix Workshop",
   tagline: "Build your playlist manager",
   favicon: "img/favicon.ico",
 
