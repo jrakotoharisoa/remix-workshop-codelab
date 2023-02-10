d27634f029dfabcd2c1cf174b6931aef6b0028f3
Johann Rakotoharisoa <johann.rakotoharisoa@gmail.com>
2023-02-10 15:26:21 +0100

    ✏️ fix typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Remix Workshop",
-  tagline: "Développons un peu outils de gestion de playlist",
+  tagline: "Développons un petit outil de gestion de playlist",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
